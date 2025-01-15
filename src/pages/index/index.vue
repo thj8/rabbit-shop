8bcaeff7ab5cb06d639e56d62ecaefa3c3a23d45
tanghaijun <tanghj@cyberpeace.cn>
2025-01-15 22:03:06 +0800

    热门推荐模块渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import { getHomeBannerAPI, getCategroyPanelAPI } from '@/services/home'
+import { getHomeBannerAPI, getCategroyPanelAPI, getHotPanelAPI } from '@/services/home'
+
 import CustomNavbar from './components/CustomNavbar.vue'
 import CategroyPanel from './components/CategroyPanel.vue'
+import HotPanel from './components/HotPanel.vue'
+
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
-import type { BannerItem, CategoryItem } from '@/types/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 
 // 获取轮播数据
 const bannerList = ref<BannerItem[]>([])
@@ -20,16 +23,32 @@ const getCategroyPanelData = async () => {
   categoryList.value = res.result
 }
 
+// 获取热门推荐
+const hotList = ref<HotItem[]>()
+const getHotPanelData = async () => {
+  const res = await getHotPanelAPI()
+  hotList.value = res.result
+}
+
 onLoad(() => {
   getHomeBannerData()
   getCategroyPanelData()
+  getHotPanelData()
 })
 </script>
 
 <template>
+  <!--导航栏-->
   <CustomNavbar></CustomNavbar>
+
+  <!--轮播-->
   <XtxSwiper :list="bannerList"></XtxSwiper>
+
+  <!--分类面板-->
   <CategroyPanel :list="categoryList"></CategroyPanel>
+
+  <!--热门推荐-->
+  <HotPanel :list="hotList"></HotPanel>
 </template>
 
 <style lang="scss">
--- src/services/home.ts
+++ src/services/home.ts
@@ -1,4 +1,4 @@
-import type { CategoryItem, BannerItem } from '@/types/home'
+import type { CategoryItem, BannerItem, HotItem } from '@/types/home'
 import { http } from '@/utils/http'
 
 export const getHomeBannerAPI = (distributionSite = 1) => {
@@ -15,5 +15,12 @@ export const getCategroyPanelAPI = () => {
   return http<CategoryItem[]>({
     method: 'GET',
     url: '/home/category/mutli',
+  })
+}
+
+export const getHotPanelAPI = () => {
+  return http<HotItem[]>({
+    method: 'GET',
+    url: '/home/hot/mutli',
   })
 }
