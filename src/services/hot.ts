2b27135cf6ad4b6e3e3512977faa073d4c809050
tanghaijun <tanghj@cyberpeace.cn>
2025-01-17 22:01:54 +0800

    热门推荐基本渲染成功

--- src/services/hot.ts
+++ src/services/hot.ts
@@ -1,11 +1,12 @@
 import type { PageParams } from '@/types/global'
+import type { HotResult } from '@/types/hot'
 import { http } from '@/utils/http'
 
 type HotParams = PageParams & { subType?: string }
 
 //通用热门推荐类型
 export const getHotRecommendAPI = (url: string, data?: HotParams) => {
-  return http({
+  return http<HotResult>({
     method: 'GET',
     url: url,
     data,
