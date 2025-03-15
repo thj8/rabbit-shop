28e3fc10327fbfbe12bcc8e19725c64fd4295731
tanghaijun <tanghj@cyberpeace.cn>
2025-03-15 16:00:08 +0800

    个人信息展示

--- src/types/member.d.ts
+++ src/types/member.d.ts
@@ -1,15 +1,33 @@
-/** 小程序登录 登录用户信息 */
-export type LoginResult = {
+/** 用户基础信息 */
+type BaseProfile = {
   /** 用户ID */
   id: number
-  /** 头像  */
+  /** 头像 */
   avatar: string
-  /** 账户名  */
-  account: string
   /** 昵称 */
-  nickname?: string
+  nickname: string | null
+  /** 账号名称 */
+  account: string
+}
+
+/** 小程序登录 登录用户信息 */
+export type LoginResult = BaseProfile & {
   /** 手机号 */
   mobile: string
   /** 登录凭证 */
   token: string
+}
+
+export type Gender = '男' | '女'
+
+/** 用户个人信息 */
+export type ProfileDetail = BaseProfile & {
+  /** 性别 */
+  gender: Gender
+  /** 生日 */
+  birthday: string | null
+  /** 省市区 */
+  fullLocation: string
+  /** 职业 */
+  profession: string | null
 }
